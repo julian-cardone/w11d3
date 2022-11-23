aa93c1d44e92de456593771073b52be2c4d9a351
Jeffrey Cheng <jeffreych6@gmail.com>
2022-11-23 17:12:53 -0500

    Phase 5 WIP

--- practice-for-week-15-react-redux-grocery-store-long-practice/src/components/Cart/CartItem.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice/src/components/Cart/CartItem.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { removeCart, addCart, decrementCart, updateCart } from "../../store/cart";
 
 function CartItem({ item }) {
   const [count, setCount] = useState(item.count);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     setCount(item.count);
@@ -13,20 +16,28 @@ function CartItem({ item }) {
       <div className="cart-item-menu">
         <input
           type="number"
-          value={count}
+          defaultValue={count}
+          onChange={()=>{
+            dispatch(updateCart(item.id, item.count))
+            setCount(item.count);
+          }} // not working yet
         />
         <button
           className="cart-item-button"
+          onClick={()=>dispatch(addCart(item.id)) }
         >
           +
         </button>
         <button
           className="cart-item-button"
+          onClick={()=>dispatch(decrementCart(item.id)) }
+
         >
           -
         </button>
         <button
           className="cart-item-button"
+          onClick={()=>dispatch(removeCart(item.id)) }
         >
           Remove
         </button>
--- practice-for-week-15-react-redux-grocery-store-long-practice/src/store/cart.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice/src/store/cart.js
@@ -1,9 +1,34 @@
 const ADDTOCART = "ADDTOCART";
+const REMOVEFROMCART = "REMOVEFROMCART";
+const DECREMENTCART = "DECREMENTCART"
+const UPDATECART = "UPDATECART"
 
 export const addCart = item => {
     return {
         type: ADDTOCART,
         id: item
+    }
+}
+
+export const decrementCart = item => {
+    return {
+        type: DECREMENTCART,
+        id: item
+    }
+}
+
+export const removeCart = itemId => {
+    return {
+        type: REMOVEFROMCART,
+        id: itemId
+    }
+}
+
+export const updateCart = (itemId, itemCount) => {
+    return {
+        type: UPDATECART,
+        id: itemId,
+        count: itemCount
     }
 }
 
@@ -13,10 +38,16 @@ export default function cartReducer(state = {}, action) {
 
     switch (action.type) {
         case ADDTOCART:
-            nextState[action.id] = {
-                id: action.id,
-                count: 1
-            }
+            nextState[action.id] ? nextState[action.id].count += 1 : nextState[action.id] = {id: action.id, count: 1}
+            return nextState;
+        case REMOVEFROMCART:
+            delete nextState[action.id]
+            return nextState;
+        case DECREMENTCART:
+            nextState[action.id].count === 1 ? delete nextState[action.id] : nextState[action.id].count -= 1
+            return nextState;
+        case UPDATECART:
+            nextState[action.id].count = action.count
             return nextState;
         default:
             return state;
